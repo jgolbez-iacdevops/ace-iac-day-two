d644ec506973e83783fcfa3eaa9a60ec86cf3ec7
jgolbez-iacdevops <113311210+jgolbez-iacdevops@users.noreply.github.com>
2022-09-12 14:29:12 -0400

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,7 +1,7 @@
 terraform {
   required_version = ">= 1.1.0"
   backend "remote" {
-    # organization = "jgolbez-iac"
+    organization = "jgolbez-iac"
     workspaces {
       name = "ace-iac-day-two"
     }
